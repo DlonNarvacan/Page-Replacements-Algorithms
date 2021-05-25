97dbf6dae1166dcae0fd3992fbb4f2a70b2b761e
Arnold Narvacan <bignetnarvacan@yahoo.com>
2021-05-25 21:55:37 +0800

    Finished: No charts

--- mfu.java
+++ mfu.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class mfu {
     public static void main(String[] args) {
         Scanner myinp = new Scanner(System.in);
-        int references, frames, fault_count=0, fno, checker=0;
+        int references, frames, fault_count=0, checker=0;
 
 
         System.out.print("Number of Frames: ");
@@ -27,51 +27,52 @@ public class mfu {
         }
 
         for (int i = 0; i < reference_string.length; i++) {
-            int temp = 0;
-
-            for (int j = 0; j < frames; j++) {
-                if (pno[j] == reference_string[i]) {
-                    temp = j;        
-                } else {
-                    temp = -1;
-                }
-            }
-            
-            checker = temp;
-            
+            checker = capture_page(reference_string[i], frames, pno);
             if (checker == -1) {
-                
-                int temp1 = 0;
-
-                for (int j = 0; j <= frames; j++) {
-                    if (pno[j] == -1) {
-                        temp1 = j;
-                    } else {
-                        temp1 = -1;
-                    }
+                checker = fframe(frames, pno);
+                if (checker  == -1) {
+                    checker = mframe(frames, frequency);
                 }
 
-                checker = temp1;
-
-                if (checker == -1) {
-                    
-                    int temp2 = 0;
-
-                    for (int j = 1; j < frames; j++) {
-                        if (frequency[j] > frequency[temp2]) {
-                            temp2 = j;
-                        }
-                    }
-
-                    checker = temp2;
-
-                    fault_count++;
-                    pno[checker] = reference_string[i];
-                }
-
+                fault_count++;
+                pno[checker] = reference_string[i];
+                frequency[checker] = 1;
             } else {
-                
+                frequency[checker]++;
             }
         }
+        System.out.print("\n===============================");
+        System.out.println("\nFault: "+fault_count);
+
+    }
+
+    static int capture_page(int page, int frames, int pno[]){
+        for (int i = 0; i < frames; i++) {
+            if (pno[i] == page) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int fframe(int frames, int pno[]){
+        for (int i = 0; i <= frames; i++) {
+            if (pno[i] == -1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int mframe(int frames, int frequency[]){
+        int short1 = 0;
+        for (int i = 1; i < frames; i++) {
+            if (frequency[i] > frequency[short1]) {
+                short1 = i;
+            }
+        }
+        return short1;
     }
 }
+
+
